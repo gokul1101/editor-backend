8c1928e40f2e2c4218ded9b009825695800055b8
Gajendhiranm <deepakjs1234@gmail.com>
2021-12-16 16:49:13 +0530

    UI-FIX

--- client/src/components/Student/Main/Profile/Profile.jsx
+++ client/src/components/Student/Main/Profile/Profile.jsx
@@ -3,6 +3,7 @@ import "./Profile.css";
 import InputReducer from "../../../Reducer/InputReducer";
 import SelectReducer from "../../../Reducer/SelectReducer/SelectReducer";
 import { Button, makeStyles } from "@material-ui/core";
+import { useState } from "react";
 const useStyles = makeStyles((theme) => ({
   root: {
     border: "1px solid #1E2D64",
@@ -26,100 +27,111 @@ const useStyles = makeStyles((theme) => ({
 }));
 const Profile = (props) => {
   const styles = useStyles()
+  const[user,setUser] = useState({
+    email:"",
+    register_number:"",
+    old_password:"",
+    new_password:"",
+    name:"",
+    phone_number:"",
+    gender:""
+  })
   useEffect(() => {
     props.setSideToggle(false);
   });
   return (
-    <div className={styles.margin}>
-      <div>
-        <p className="text-left dash-title-category">Edit Details</p>
-        <div className="col p-0" style={{ marginTop: "-20px" }}>
-          <div className="hr">
-            <hr className="col p-0" />
-          </div>
-          <div className="col question-outoff p-0 mb-3">
-            <span>Manage your personal information , password and more</span>
-          </div>
-        </div>
-      </div>
-      <div className="input-container w-100 d-flex flex-column">
-        <div className="d-flex mt-2 mb-2">
-          <div className="col-md-6">
-            <InputReducer placeholder="Enter Email" name="Email" type="email" label="Email"  className={styles.fieldColor}/>
-          </div>
-          <div className="col-md-6">
-            <InputReducer
-              placeholder="Register Number"
-              name="Register no"
-              type="text"
-              label="Register Number"
-              className={styles.fieldColor}
-            />
+    <div className="container">
+      <div className={styles.margin}>
+        <div>
+          <p className="containertext-left dash-title-category">Edit Details</p>
+          <div className="col p-0" style={{ marginTop: "-20px" }}>
+            <div className="hr">
+              <hr className="col p-0" />
+            </div>
+            <div className="col question-outoff p-0 mb-3">
+              <span>Manage your personal information , password and more</span>
+            </div>
           </div>
         </div>
-        <div className="d-flex mt-2 mb-2">
-          <div className="col-md-6">
-            <InputReducer
-              placeholder="Old passoword"
-              name="Old Password"
-              type="password"
-              label="Old Password"
-              className={styles.fieldColor}
-            />
+        <div className="input-container w-100 d-flex flex-column">
+          <div className="d-flex mt-2 mb-2">
+            <div className="col-md-6">
+              <InputReducer placeholder="Enter Email" name="Email" type="email" label="Email"  className={styles.fieldColor}/>
+            </div>
+            <div className="col-md-6">
+              <InputReducer
+                placeholder="Register Number"
+                name="Register no"
+                type="text"
+                label="Register Number"
+                className={styles.fieldColor}
+              />
+            </div>
           </div>
-          <div className="col-md-6">
-            <InputReducer
-              placeholder="New passoword"
-              name="New Password"
-              label="New Password"
-              type="password"
-              className={styles.fieldColor}
-            />
+          <div className="d-flex mt-2 mb-2">
+            <div className="col-md-6">
+              <InputReducer
+                placeholder="Old passoword"
+                name="Old Password"
+                type="password"
+                label="Old Password"
+                className={styles.fieldColor}
+              />
+            </div>
+            <div className="col-md-6">
+              <InputReducer
+                placeholder="New passoword"
+                name="New Password"
+                label="New Password"
+                type="password"
+                className={styles.fieldColor}
+              />
+            </div>
           </div>
-        </div>
-        <div className="d-flex mt-2 mb-2">
-          <div className="col-md-6">
-            <InputReducer placeholder="Enter Name" name="Name" type="text" label="Name" className={styles.fieldColor} />
+          <div className="d-flex mt-2 mb-2">
+            <div className="col-md-6">
+              <InputReducer placeholder="Enter Name" name="Name" type="text" label="Name" className={styles.fieldColor} />
+            </div>
+            <div className="col-md-6">
+              <InputReducer
+                placeholder="Enter Phone Nuber"
+                name="Phone number"
+                label="Phone number"
+                type="Number"
+                className={styles.fieldColor}
+              />
+            </div>
           </div>
-          <div className="col-md-6">
-            <InputReducer
-              placeholder="Enter Phone Nuber"
-              name="Phone number"
-              label="Phone number"
-              type="Number"
-              className={styles.fieldColor}
-            />
-          </div>
-        </div>
-        <div
-          className="d-flex mt-2 mb-2"
-          style={{ position: "relative", left: "-7px" }}
-        >
-          <div className="col-md-6 ml-2">
-            <SelectReducer
-              array={["MALE", "FEMALE"]}
-              name="Select gender"
+          <div
+            className="d-flex mt-2 mb-2"
+            style={{ position: "relative", left: "-7px" }}
+          >
+            <div className="col-md-6 ml-2">
+              <SelectReducer
+                array={["MALE", "FEMALE"]}
+                name="Select gender"
+              
+                className={styles.fieldColor}
+              />
+            </div>
             
-              className={styles.fieldColor}
-            />
           </div>
-          
-        </div>
-        {/* <div
-          className="d-flex mt-3 mb-2"
-          style={{ position: "relative", left: "-7px" }}
-        >
-          <div className="col-md-6">
-            <SelectReducer array={["CSE", "IT", "CIVIL"]} className="w-100" name="Course name" />
+          {/* <div
+            className="d-flex mt-3 mb-2"
+            style={{ position: "relative", left: "-7px" }}
+          >
+            <div className="col-md-6">
+              <SelectReducer array={["CSE", "IT", "CIVIL"]} className="w-100" name="Course name" />
+            </div>
+          </div> */}
+          <div className="d-flex pl-3 mt-2">
+            <Button   className="mr-2 btn btn-save">
+              Save Changes
+            </Button>
+            <Button className="btn-cancel">
+              Cancel
+            </Button>
           </div>
-        </div> */}
-        <div className="d-flex pl-3 mt-2">
-          <Button   className="mr-2 btn btn-save">
-            Save Changes
-          </Button>
-          <Button className="btn-cancel">
-            Cancel
-          </Button>
         </div>
       </div>
     </div>
