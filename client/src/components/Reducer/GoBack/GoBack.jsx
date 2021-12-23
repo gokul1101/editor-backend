605b7a7042e86522854992631003430f0d8694c1
vasanthanp <dhanukarthick15@gmail.com>
2021-12-23 14:34:52 +0530

    reusable dialog component

--- client/src/components/Reducer/GoBack/GoBack.jsx
+++ client/src/components/Reducer/GoBack/GoBack.jsx
@@ -1,12 +1,7 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
 const GoBack = (props) => {
-  let history = useHistory();
   return (
-    <div
-      className="back-btn mt-3 ml-4"
-      onClick={props.onClickHandler}
-    >
+    <div className="back-btn mt-3 ml-4" onClick={props.onClickHandler}>
       <div className="triangle"></div>
       <div className="halfcircle"></div>
     </div>
