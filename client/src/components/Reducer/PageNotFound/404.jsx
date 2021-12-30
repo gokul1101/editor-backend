70d9fec221cbd0d2ea0aa25e32c6be3a9df3d470
vasanthanp <dhanukarthick15@gmail.com>
2021-12-30 14:20:55 +0530

    404 page

--- client/src/components/Reducer/PageNotFound/404.jsx
+++ client/src/components/Reducer/PageNotFound/404.jsx
@@ -3,7 +3,7 @@ import NoMatch from "../../Images/PageNotFound.webp";
 const PageNotFound = () => {
   return (
     <div className="container d-flex align-items-center justify-content-center h-100">
-      <img src={NoMatch} alt="PageNotFound" className="img-fluid " />
+      <img src={NoMatch} alt="PageNotFound" className="img-fluid" />
     </div>
   );
 };
